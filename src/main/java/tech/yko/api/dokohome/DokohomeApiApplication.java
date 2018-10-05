adff88c8bfd3cc241ef021ef31611ce6fb0a9240
maruhachi <konchan1129@gmail.com>
2018-10-05 21:15:51 +0900

    initial commit. spring initializr.

---
+++ src/main/java/tech/yko/api/dokohome/DokohomeApiApplication.java
@@ -0,0 +1,12 @@
+package tech.yko.api.dokohome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokohomeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokohomeApiApplication.class, args);
+	}
+}
